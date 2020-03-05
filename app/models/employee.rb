8511f8f68901836bf01b012097aad3b01ee6a881
brewchetta <chett.tiller@gmail.com>
2020-03-05 13:23:59 -0500

    Departments have employees as intended?

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,3 +1,4 @@
 class Employee < ActiveRecord::Base
     belongs_to :manager
+    belongs_to :department
 end
--- db/migrate/20200303204950_create_employees.rb
+++ db/migrate/20200303204950_create_employees.rb
@@ -3,6 +3,7 @@ class CreateEmployees < ActiveRecord::Migration[6.0]
     create_table :employees do |t|
       t.string :name
       t.integer :manager_id
+      t.integer :department_id
     end
   end
 end
